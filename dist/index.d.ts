1402bdae6a1c59a81982bfa1eac69ac112a93256
Bogdan Kapustin <goldbranch.dev@gmail.com>
2025-03-27 21:32:48 +0200

    fix: remove redundant dependencies

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,3 @@
-import { SolanaConnectLight } from "./adapter/wallet_connection/SolanaConnectLight";
-import { SolanaConnectConfig } from "./adapter/wallet_connection/SolanaConnectConfig";
-import { WalletAdapterLight, SignatureResult, ErrorAdapterNotConaintSignInMethod, ErrorUserDeclinedSignIn } from "./adapter/WalletAdapter";
-import { Header, Signature } from "@web3auth/sign-in-with-solana";
-export { SolanaConnectLight, SolanaConnectConfig, Header, Signature, ErrorUserDeclinedSignIn, ErrorAdapterNotConaintSignInMethod, SignatureResult, WalletAdapterLight, };
+export { SolanaConnectLight } from "./adapter/wallet_connection/SolanaConnectLight";
+export { SolanaConnectConfig } from "./adapter/wallet_connection/SolanaConnectConfig";
+export { WalletAdapterLight, SignatureResult, ErrorAdapterNotConaintSignInMethod, ErrorUserDeclinedSignIn, } from "./adapter/WalletAdapter";
--- lib/index.ts
+++ lib/index.ts
@@ -1,31 +1,8 @@
-import { SolanaConnectLight } from "./adapter/wallet_connection/SolanaConnectLight"
-import { SolanaConnectConfig } from "./adapter/wallet_connection/SolanaConnectConfig"
-import { WalletAdapterLight, SignatureResult, ErrorAdapterNotConaintSignInMethod,ErrorUserDeclinedSignIn } from "./adapter/WalletAdapter";
-import { Header, Signature } from "@web3auth/sign-in-with-solana";
-
-
+export { SolanaConnectLight } from "./adapter/wallet_connection/SolanaConnectLight";
+export { SolanaConnectConfig } from "./adapter/wallet_connection/SolanaConnectConfig";
 export {
-    SolanaConnectLight, 
-    SolanaConnectConfig,
-    Header, 
-    Signature,
-
-    ErrorUserDeclinedSignIn,
-    ErrorAdapterNotConaintSignInMethod,
-    SignatureResult,
-    WalletAdapterLight, 
-}
-
-// console.log(module)
-
-// module.exports = {
-//     module: {
-//         rules: [
-//             {
-//             test: /\.css$/,
-//             use: ['style-loader', 'css-loader'],
-//             }
-//         ]
-//     }
-// }
-
+  WalletAdapterLight,
+  SignatureResult,
+  ErrorAdapterNotConaintSignInMethod,
+  ErrorUserDeclinedSignIn,
+} from "./adapter/WalletAdapter";
